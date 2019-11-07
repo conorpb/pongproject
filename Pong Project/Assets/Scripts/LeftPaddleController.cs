e73147a22cb327182aed49bb11bded7513113b2b
Conor Brennan <conor.brennan@digipen.edu>
2019-11-07 13:49:06 -0800

    left and right paddle controllers

---
+++ Pong Project/Assets/Scripts/LeftPaddleController.cs
@@ -0,0 +1,32 @@
+﻿/* 
+ By: Conor Brennan
+ Date: 11/7/19
+ Description: Up and down controlls for the left paddle.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftPaddleController : MonoBehaviour
+{
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+    }
+}
