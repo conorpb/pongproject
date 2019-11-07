c885761f479a7b114432a895aaef9890fbbd30e7
Bluephaser <liam.binford@digipen.edu>
2019-11-07 14:25:55 -0800

    Added Ball Launch script

---
+++ Pong Project/Assets/Scripts/LaunchBall.cs
@@ -0,0 +1,35 @@
+﻿/*
+ By: Liam Binford
+ Date: 11/7/19
+ Description: Porgram for launching the ball at the beginning of the round
+ */
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchBall : MonoBehaviour
+{
+    public float speed = 10;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Vector2 dir = Random.insideUnitCircle;
+
+            rb.velocity = speed * dir;
+        }
+    }
+}
