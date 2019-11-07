b906d68d774cbb5ff4fd0b8710ed98343fa9a13f
Bluephaser <liam.binford@digipen.edu>
2019-11-07 13:22:10 -0800

    Finished most of main menu

---
+++ Pong Project/Assets/Sprites/SwitchLevel.cs
@@ -0,0 +1,32 @@
+﻿/*
+ By: Liam Binford
+ Date: 11/7/19
+ Description: Script applied to things that change the scene
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevel : MonoBehaviour
+{
+    public string NextScene = "Level1";
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(NextScene);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
